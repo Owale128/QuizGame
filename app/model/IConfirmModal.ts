e61ce235332c18b24a659c0ce871bbf492bd1945
Owale128 <Christian.buaka@hotmail.com>
2025-12-13 23:50:23 +0100

    add IConfirmModal

---
+++ app/model/IConfirmModal.ts
@@ -0,0 +1,9 @@
+export interface IConfirmModal {
+    isOpen: boolean;
+    title: string;
+    message: string;
+    onConfirm: () => void;
+    onCancel: () => void;
+    confirmText?: string;
+    cancelText?: string;
+}
