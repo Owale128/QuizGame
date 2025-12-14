7f957202c1399544e82e2a01c88a7b4850c60ae4
Owale128 <Christian.buaka@hotmail.com>
2025-12-14 11:19:43 +0100

    add dynamic rendering to API routes

--- app/api/highscores/route.ts
+++ app/api/highscores/route.ts
@@ -1,5 +1,7 @@
 import { getHighscore } from "@/app/lib/getHighscore"
 
+export const dynamic = 'force-dynamic'
+
 export const GET = async () => {
    return getHighscore()
 }
--- app/api/score/route.ts
+++ app/api/score/route.ts
@@ -2,8 +2,10 @@ import { NextRequest } from "next/server"
 import { getScore } from "@/app/lib/getScore";
 import { postScore } from "@/app/lib/postScore";
 
+export const dynamic = 'force-dynamic'
+
 export const POST = async (req: NextRequest) => {
-return postScore(req)   
+return postScore(req)
 }
 
 export const GET = async (req: NextRequest) => {
